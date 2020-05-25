8e16457aeeacc715c25cffc51e836df353d119e9
Nicolas Rodriguez <nicoladmin@free.fr>
2020-05-25 15:56:24 +0200

    Use whitespaces, not tabs

--- typings/bootstrap-notify/notify.d.ts
+++ typings/bootstrap-notify/notify.d.ts
@@ -3,63 +3,63 @@
 /* tslint:disable: interface-name no-any */
 
 interface JQueryStatic {
-	/* tslint:enable: interface-name */
-	notify(message: string): INotifyReturn;
-	notify(opts: INotifyOptions, settings?: INotifySettings): INotifyReturn;
-	notifyDefaults(settings: INotifySettings): void;
-	notifyClose(): void;
-	notifyClose(command: string): void;
+  /* tslint:enable: interface-name */
+  notify(message: string): INotifyReturn;
+  notify(opts: INotifyOptions, settings?: INotifySettings): INotifyReturn;
+  notifyDefaults(settings: INotifySettings): void;
+  notifyClose(): void;
+  notifyClose(command: string): void;
 }
 
 interface INotifyOptions {
-	message: string;
-	title?: string;
-	icon?: string;
-	url?: string;
-	target?: string;
+  message: string;
+  title?: string;
+  icon?: string;
+  url?: string;
+  target?: string;
 }
 
 interface INotifySettings {
-	element?: string;
-	position?: string;
-	type?: string;
-	allow_dismiss?: boolean;
-	allow_duplicates?: boolean;
-	newest_on_top?: boolean;
-	showProgressbar?: boolean;
-	placement?: {
-		from?: string;
-		align?: string;
-	};
-	offset?: number;
-	spacing?: number;
-	z_index?: number;
-	delay?: number;
-	timer?: number;
-	url_target?: string;
-	mouse_over?: Function;
-	animate?: {
-		enter?: string;
-		exit?: string;
-	};
-	onShow?: () => void;
-	onShown?: () => void;
-	onClose?: () => void;
-	onClosed?: () => void;
-	icon_type?: string;
-	template?: string;
+  element?: string;
+  position?: string;
+  type?: string;
+  allow_dismiss?: boolean;
+  allow_duplicates?: boolean;
+  newest_on_top?: boolean;
+  showProgressbar?: boolean;
+  placement?: {
+    from?: string;
+    align?: string;
+  };
+  offset?: number;
+  spacing?: number;
+  z_index?: number;
+  delay?: number;
+  timer?: number;
+  url_target?: string;
+  mouse_over?: Function;
+  animate?: {
+    enter?: string;
+    exit?: string;
+  };
+  onShow?: () => void;
+  onShown?: () => void;
+  onClose?: () => void;
+  onClosed?: () => void;
+  icon_type?: string;
+  template?: string;
 }
 
 interface UpdateCommands {
-	type?: string;
-	progress?: number;
-	message?: string;
-	icon?: string;
+  type?: string;
+  progress?: number;
+  message?: string;
+  icon?: string;
 }
 
 interface NotifyReturn {
-	$ele: JQueryStatic;
-	close(): void;
-	update(commands: UpdateCommands|Record<string, any>): void;
-	update(command: string, update: any): void;
+  $ele: JQueryStatic;
+  close(): void;
+  update(commands: UpdateCommands|Record<string, any>): void;
+  update(command: string, update: any): void;
 }
